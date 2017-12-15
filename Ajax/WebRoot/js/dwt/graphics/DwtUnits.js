7c64981a3c8dc75f09eaa1cec8ea3b236c11ed3c
John Holder <john@johnholder.net>
2017-12-14 20:23:10 -0800

    commit for branch judaspriest-850-beta3-foss

---
+++ Ajax/WebRoot/js/dwt/graphics/DwtUnits.js
@@ -0,0 +1,32 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * 
+ * Zimbra Collaboration Suite Web Client
+ * Copyright (C) 2005, 2006 Zimbra, Inc.
+ * 
+ * The contents of this file are subject to the Yahoo! Public License
+ * Version 1.0 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * 
+ * ***** END LICENSE BLOCK *****
+ */
+
+
+function DwtUnits() {
+}
+
+DwtUnits.PIXEL_UNIT = "px";
+DwtUnits.CM_UNIT = "cm";
+DwtUnits.MM_UNIT = "mm";
+DwtUnits.INCH_UNIT = "in";
+DwtUnits.PCT_UNIT = "%";
+DwtUnits.POINT = "pt";
+
+// pixel widths
+DwtUnits.WIDTH_EM = AjxEnv.isIE ? 9 : 11; // width of "m"
+DwtUnits.WIDTH_SEP = AjxEnv.isIE ? 6 : 8; // width of ", "
+DwtUnits.WIDTH_ELLIPSIS = 15;			 // width of " ... "
