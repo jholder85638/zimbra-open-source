032bac98db567fcb71401fc70fbdc86b73978a55
John Holder <john@johnholder.net>
2017-12-14 19:33:58 -0800

    commit for branch franklin-509-foss

---
+++ Ajax/WebRoot/examples/dataViewer/config/msgs/DvMsg_en.js
@@ -0,0 +1,36 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * 
+ * Zimbra Collaboration Suite Web Client
+ * Copyright (C) 2005, 2006 Zimbra, Inc.
+ * 
+ * The contents of this file are subject to the Yahoo! Public License
+ * Version 1.0 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * 
+ * ***** END LICENSE BLOCK *****
+ */
+
+
+function DvMsg() {}
+
+DvMsg.after = "After";
+DvMsg.and = "and";
+DvMsg.applyFilter = "Apply Filter";
+DvMsg.before = "Before";
+DvMsg.between = "Between";
+DvMsg.contains = "Contains";
+DvMsg.equals = "Equals";
+DvMsg.dblClick = "You have actioned the item with a $0 of $1";
+DvMsg.filteredRecords = "Filtered Records";
+DvMsg.greaterThan = "Greater than";
+DvMsg.lessThan = "Less than";
+DvMsg.matches = "Matches";
+DvMsg.no = "No";
+DvMsg.originalData = "Original Data";
+DvMsg.totalRecords = "Total Records";
+DvMsg.yes = "Yes";
